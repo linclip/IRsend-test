1b6b1c0cc47e7b68fc58cdbbd400825d3fa044c6
linclip <linclip@gmail.com>
2019-04-06 23:17:35 +0900

    add lib url

--- IRsend-test.ino
+++ IRsend-test.ino
@@ -1,7 +1,8 @@
 /*
- * 赤外線リモコンテスト
- * 2019/04/05
+ * 赤外線リモコンテスト 2019/04/05
+ * https://github.com/z3t0/Arduino-IRremote
  */
+
 #include <IRremote.h>
 byte SEND_PIN = 9;
 IRsend irsend(SEND_PIN);
@@ -11,42 +12,21 @@ IRsend irsend(SEND_PIN);
 
 bool led = true;
 
-int IO_1 = 0;
-int IO_2 = 26;
-int IO_3 = 36;
-
 void setup()
 {
-  // put your setup code here, to run once:
   M5.begin();
   M5.Lcd.fillScreen(BLACK);
 
-  M5.Axp.ScreenBreath(9); //7 - 16
+  M5.Axp.ScreenBreath(9); //7 - 16 暗めにしたい
 
-  M5.Lcd.setTextSize(2); //3
+  M5.Lcd.setTextSize(2);
   M5.Lcd.setCursor(4, 40);
   M5.Lcd.print("M5 IR");
-  //M5.Lcd.printf("%2d On", buttonPins[i]);
 
   //start serial connection
   Serial.begin(9600);
 
-  /*
-  //!Screen
-  M5.Lcd.fillScreen(BLUE);
-  delay(50);
-  M5.Lcd.fillScreen(RED);
-  delay(50);
-  M5.Lcd.fillScreen(YELLOW);
-  delay(50);
-  M5.Lcd.fillScreen(ORANGE);
-  delay(50);
-  M5.Lcd.fillScreen(WHITE);
-  delay(50);
-  */
-
-  //!IR LED
-  //rem.begin(M5_IR,true);
+  // LED
   pinMode(M5_LED, OUTPUT);
   digitalWrite(M5_LED, LOW); //これで点灯
   digitalWrite(M5_LED, HIGH); //これで消灯
